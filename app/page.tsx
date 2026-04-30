d528747749194f9b7a6feee7fe5725a696ce3b64
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2026-04-30 17:38:05 +0400

    feat: Implement dark mode support and update UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to the Home Page</h1>
+      <h1 className="inline-flex flex-col text-9xl">
+        <span className="leading-[1]">LoveFrom</span>
+        <span className="leading-[1]">Games</span>
+      </h1>
     </div>
   );
 }
