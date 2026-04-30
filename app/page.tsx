089fadd070d33ebc5b1fa9a4a8bf82b26efc7013
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2026-04-30 22:07:24 +0400

    feat: add footer components and social links
    - Implemented AppStoreButtons component for app download links.
    - Created SocialLinks component to display various social media icons.
    - Updated Home page to include social links and commented out HeroHeading and AppStoreButtons for future use.
    - Added SVG icons for Apple, Play Store, Facebook, Instagram, TikTok, Telegram, X, and YouTube.
    - Introduced a basic Footer component structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import { HeroHeading, Logo } from "@/components";
+import { AppStoreButtons } from "@/components/footer/app-store-Buttons";
+import { SocialLinks } from "@/components/footer/social-links";
+import AppleLogo from "@/public/icons/apple-logo.svg";
+import PlayStoreLogo from "@/public/icons/play-store.svg";
 
 export default function Home() {
   return (
     <main className="flex-1">
       {/* <Logo /> */}
-      <HeroHeading />
+      {/* <HeroHeading /> */}
+      {/* <AppStoreButtons
+        logo={AppleLogo}
+        text="Download on the"
+        title="App Store"
+      />
+      <AppStoreButtons
+        logo={PlayStoreLogo}
+        text="Get it on"
+        title="PLay Store"
+      /> */}
+
+      <SocialLinks />
     </main>
   );
 }
