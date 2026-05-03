c1eeddbcfd34e1354c622677803fbbdda039a526
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2026-05-03 18:50:39 +0400

    feat: add Prettier configuration and plugins for code formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindStylesheet: "./app/globals.css",
+  tailwindFunctions: ["clsx", "cn"],
+};
+
+export default config;
