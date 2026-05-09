1b82f6efefac16c1923664065d6662f7a891b3c2
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2026-05-09 20:34:30 +0400

    fix: adjust max-width and padding for responsive design in About, Privacy, and Terms pages

--- components/slider-button.tsx
+++ components/slider-button.tsx
@@ -12,7 +12,7 @@ export function SliderButton() {
 
   return (
     <div className="fixed top-0 left-0 z-10 flex h-dvh flex-col items-center justify-center">
-      <button className="group ml-11 hidden h-14 w-14 cursor-pointer items-center justify-center rounded-full border-2 transition-colors duration-500 ease-in-out xl:flex xl:hover:bg-foreground">
+      <button className="group ml-1 2xl:ml-11 hidden h-14 w-14 cursor-pointer items-center justify-center rounded-full border-2 transition-colors duration-500 ease-in-out xl:flex xl:hover:bg-foreground">
         <Link className="p-4.5 pr-5" href="/">
           <Image
             src={SliderArrow}
